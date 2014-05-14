f9a2f3b315f790307b32321324ea57114fbf27d8
Daniel Shusta <pair+dshusta@pivotallabs.com>
2014-05-14 16:01:25 -0700

    Initial project creation

---
+++ Collisions/Collisions/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Collisions
+//
+//  Created by pivotal on 5/14/14.
+//  Copyright (c) 2014 Shusta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Collisions/Collisions/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Collisions
+//
+//  Created by pivotal on 5/14/14.
+//  Copyright (c) 2014 Shusta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
